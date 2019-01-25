f30737770b9b92fccbe2beec0b26a0d01c0169be
Ali Javadi <ajavadia@princeton.edu>
2019-01-25 11:43:37 -0500

    mention support for python 3.7 in setup.py

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering",
     ],
     keywords="qiskit sdk quantum",
